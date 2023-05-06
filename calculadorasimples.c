9eaaa2be09e3324030a3b866173aaaf9ac01ac60
Cássia Luz <115599591+cassialuz@users.noreply.github.com>
2023-05-06 15:50:35 -0300

    Update calculadorasimples.c

--- calculadorasimples.c
+++ calculadorasimples.c
@@ -1,11 +1,5 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h> /*biblioteca com funções de entradas e saídas*/
 #include <math.h> /*biblioteca com constantes e funções matemáticas*/
 
